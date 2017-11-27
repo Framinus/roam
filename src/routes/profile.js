2a70c804f77a75e24b7670799c6da7af4356fd72
James McCormack <thejamesmccormack@gmail.com>
2017-11-27 11:21:54 -0800

    edit profile functionality added

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const getUserProfileAndReviews = require('../db/users.js').getUserProfileAndReviews;
+const editUserProfile = require('../db/users.js').editUserProfile;
 
 router.get('/', (req, res) => {
   if (req.session.user) {
@@ -12,6 +13,14 @@ router.get('/', (req, res) => {
   } else {
     res.redirect('/');
   }
+});
+
+router.post('/', (req, res) => {
+  const { id, name, currentcity } = req.body;
+  return editUserProfile(id, name, currentcity)
+    .then(response => res.json())
+    .then(profile => profile)
+    .catch(console.error);
 });
 
 
