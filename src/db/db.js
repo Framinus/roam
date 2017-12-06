d5f7b0a29c18c6cab2309913021a1d712a8166b9
James McCormack <thejamesmccormack@gmail.com>
2017-12-05 17:47:06 -0800

    changed db connection string to or statement to allow for heroku connection

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,11 @@
 const pgp = require('pg-promise')();
 
-const db = pgp({
-  host: "localhost",
-  port: 5432,
-  database: "roam",
-});
+const db = pgp(process.env.DATABASE_URL || "postgres://localhost:5432/roam";
+
+// const db = pgp({
+//   host: "localhost",
+//   port: 5432,
+//   database: "roam",
+// });
 
 module.exports = db;
