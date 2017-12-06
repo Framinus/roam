c640d8b88c7b8169a52ae61b533ec9195f1a14d9
James McCormack <thejamesmccormack@gmail.com>
2017-12-05 17:49:06 -0800

    left out a parentheses

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const db = pgp(process.env.DATABASE_URL || "postgres://localhost:5432/roam";
+const db = pgp(process.env.DATABASE_URL || "postgres://localhost:5432/roam");
 
 // const db = pgp({
 //   host: "localhost",
